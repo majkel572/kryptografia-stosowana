b92163bc757bf12655b2b33432aa65a05e650cb8
Konrad Trusiewicz <konrad1.tr@gmail.com>
2024-10-24 23:58:22 +0200

    fixes

--- KryptografiaStosowana/BlockChainP2P.P2PNetwork.Api.Lib/PeerLib.cs
+++ KryptografiaStosowana/BlockChainP2P.P2PNetwork.Api.Lib/PeerLib.cs
@@ -4,4 +4,18 @@ public class PeerLib
 {
     public string IPAddress { get; set; }
     public string Port { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is PeerLib peer)
+        {
+            return IPAddress == peer.IPAddress && Port == peer.Port;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(IPAddress, Port);
+    }
 }
