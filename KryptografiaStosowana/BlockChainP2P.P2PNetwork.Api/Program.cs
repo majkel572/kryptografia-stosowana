3bbdedcf6cc8517c960490d71aa89e4a268a582d
Mikołaj <mrefe12345@gmail.com>
2024-11-22 02:26:07 +0100

    add swagger always

--- KryptografiaStosowana/BlockChainP2P.P2PNetwork.Api/Program.cs
+++ KryptografiaStosowana/BlockChainP2P.P2PNetwork.Api/Program.cs
@@ -33,11 +33,9 @@ builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 // app.UseHsts();
 
