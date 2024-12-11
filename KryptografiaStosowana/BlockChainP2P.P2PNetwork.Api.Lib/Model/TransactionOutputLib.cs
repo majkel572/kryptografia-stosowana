015bb6a31bc128ac5af080aa345888ef084b3c41
Mikołaj <mrefe12345@gmail.com>
2024-12-11 20:20:25 +0100

    add model

---
+++ KryptografiaStosowana/BlockChainP2P.P2PNetwork.Api.Lib/Model/TransactionOutputLib.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainP2P.P2PNetwork.Api.Lib.Model;
+
+public class TransactionOutputLib
+{
+    public string Address { get; set; }
+    public double Amount { get; set; }
+
+    public TransactionOutputLib(string address, double amount) 
+    { 
+        Address = address;
+        Amount = amount;
+    }
+}
---
+++ KryptografiaStosowana/BlockChainP2P.P2PNetwork.Api.Manager/TransactionManager.cs
@@ -0,0 +1,40 @@
+﻿using BlockChainP2P.P2PNetwork.Api.Manager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BlockChainP2P.P2PNetwork.Api.Manager;
+
+internal class TransactionManager : ITransactionManager
+{
+    public static string GetTransactionId(Transaction transaction)
+    {
+        string txInContent = string.Concat(
+            transaction.TxIns.Select(txIn => txIn.TxOutId + txIn.TxOutIndex.ToString())
+        );
+
+        string txOutContent = string.Concat(
+            transaction.TxOuts.Select(txOut => txOut.Address + txOut.Amount.ToString())
+        );
+
+        string combinedContent = txInContent + txOutContent;
+
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(combinedContent));
+            StringBuilder hashString = new StringBuilder();
+
+            foreach (byte b in hashBytes)
+            {
+                hashString.Append(b.ToString("x2")); // Converts to hexadecimal
+            }
+
+            return hashString.ToString();
+        }
+    }
+
+}
